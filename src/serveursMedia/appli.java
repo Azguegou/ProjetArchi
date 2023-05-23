da93d51afe903805686558dda6df91566b6f1939
Beowulf-0 <WahadaMehdi@gmail.com>
2023-05-23 22:34:45 +0200

    Setup du projet ArchiLog

---
+++ src/serveursMedia/appli.java
@@ -0,0 +1,22 @@
+package serveursMedia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class appli {
+	
+	
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mediatheque", "root", "");
+		System.out.println(conn.getCatalog());
+	}
+
+}
